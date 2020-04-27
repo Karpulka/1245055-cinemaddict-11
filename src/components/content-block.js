e34d9641c46030588e9d186020cc0e98a31f2d47
Alyona Karpova <karpova.dev@yandex.ru>
2020-04-27 21:59:49 +0300

    блок сортировки вынесен в отдельный компонент исправлен вывод сообщения об отсутсвии фильмов в соответствии с макетом

--- src/components/content-block.js
+++ src/components/content-block.js
@@ -3,7 +3,6 @@ import {createElement} from "../util";
 const createContentBlockTemplate = () => {
   return `<section class="films">
             <section class="films-list">
-              <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
               <div class="films-list__container">
               </div>
             </section>
